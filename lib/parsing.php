bebdedeaf8236947ef0f92a9c81a24d8a44f566c
ROllerozxa <rollerozxa@voxelmanip.se>
2023-10-05 21:47:35 +0200

    Replace poor man's scribunto with just Twig templates

--- lib/parsing.php
+++ lib/parsing.php
@@ -6,21 +6,25 @@ function parsing($text) {
 
 	$text = $markdown->text($text);
 
+	$text = str_replace('<table>', '<table class="wikitable">', $text);
+
 	$text = preg_replace_callback('@{{ ([\w\d\.]+)\((.+?)\) }}@si', 'parseFunctions', $text);
 
 	return $text;
 }
 
 function parseFunctions($match) {
-	if (str_contains($match[1], '.') || !file_exists('scripts/'.$match[1].'.lua'))
+	if (str_contains($match[1], '.') || !file_exists('templates/functions/'.$match[1].'.twig'))
 		return '<span class="error">Template error: Invalid function name</span>';
 
-	$cmd = sprintf(
-		"luajit lib/lua/bootstrap.lua %s %s 2>&1",
-	escapeshellarg($match[1]), escapeshellarg($match[2]));
+	$data = json_decode($match[2], true);
 
-	exec($cmd, $output);
-	return implode('', $output);
+	if (json_last_error_msg() != "No error")
+		return '<span class="error">Template error: '.json_last_error_msg().'</span>';
+
+	return twigloader()->render('functions/'.$match[1].'.twig', [
+		'data' => $data
+	]);
 }
 
 /**
--- scripts/thumbnail.lua
+++
@@ -1,6 +0,0 @@
-local l, json, data = ...
-
-l.write('<a href="https://%s"><img class="thumb" alt="%s" title="%s" src="https://%s"></a>',
-	data.src, data.alt, data.alt, data.src)
-
-l.output()
